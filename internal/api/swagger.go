495721cb7f8ca8a1acb724653be34a19c4ea2bf6
yukinouee <131956044+yukinouee@users.noreply.github.com>
2025-04-08 02:22:45 +0900

    fix(ui): swagger のドキュメントのパスを変更。

--- internal/api/swagger.go
+++ internal/api/swagger.go
@@ -9,7 +9,7 @@ import (
 // NewSwaggerHandler Swagger UIを提供するハンドラーを作成
 func NewSwaggerHandler() http.Handler {
 	return httpSwagger.Handler(
-		httpSwagger.URL("../doc.json"), // Swaggerドキュメントのパス
+		httpSwagger.URL("doc.json"), // Swaggerドキュメントのパス
 		httpSwagger.DeepLinking(true),
 		httpSwagger.DocExpansion("none"),
 		httpSwagger.DomID("swagger-ui"),
